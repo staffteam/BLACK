b941f174c664b6a4cfd89aac87ee0387d19ca91e
LonelyHobo <1144646550@qq.com>
2019-07-04 22:32:36 +0800

    11

--- black/src/components/home/Home.vue
+++ black/src/components/home/Home.vue
@@ -1,16 +1,56 @@
 <template>
   <div id="home">
-    
+    <!-- 首页轮播 -->
+    <div class="swiper-container" id="homeBanner">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" v-for="(item,index) in bannerData" :key="index">
+          <p><img :src="item.url" :alt="item.title"></p>
+        </div>
+      </div>
+      <!-- 如果需要分页器 -->
+      <div class="swiper-pagination"></div>
+      <!-- 如果需要导航按钮 -->
+      <div class="swiper-button-content">
+        <div class="swiper-button-prev">
+          <el-button icon="el-icon-arrow-left" circle></el-button>
+        </div>
+        <div class="swiper-button-next">
+          <el-button icon="el-icon-arrow-right" circle></el-button>
+        </div>
+      </div>
+    </div>
+    <!-- 搜索 -->
+    <div id="homeSearch">
+      <div class="l">
+        <span>热门搜索关键词：</span>
+        <a :href="item.url" v-for="(item,index) in searchData" :key="index">{{item.title}}</a>
+      </div>
+      <div class="r">
+        <p>
+          <input type="search">
+          <i class="el-icon-search"></i>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
   name: "Home",
   data() {
     return {
-      activeIndex: "1",
-      activeIndex2: "1"
+      bannerData: [
+        { title: "1", url: require("@/assets/images/home_banner.png") },
+        { title: "2", url: require("@/assets/images/home_banner.png") },
+        { title: "3", url: require("@/assets/images/home_banner.png") }
+      ],
+      searchData:[
+        {title:'脱发',url:'/search'},
+        {title:'生发',url:'/search'},
+        {title:'头发',url:'/search'},
+        {title:'乐喜力丝',url:'/search'},
+        {title:'基因育发',url:'/search'},
+      ]
     };
   },
   methods: {
@@ -19,13 +59,27 @@ export default {
     }
   },
   mounted() {
-    const test = this.msg;
-    if (test == 111) {
-      console.log(test);
-    }
+    var indexbanner = new Swiper("#homeBanner", {
+        loop: true, // 循环模式选项
+        autoplay: {
+          delay: 5000,
+          stopOnLastSlide: false,
+          disableOnInteraction: true
+        },
+        // 如果需要分页器
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        },
+
+        // 如果需要前进后退按钮
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        }
+      });
   }
 };
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="less" src="../../../src/assets/less/home.less"></style>
+<style scoped lang="less" src="../../assets/less/home.less"></style>
--- black/src/components/leader/Header.vue
+++ black/src/components/leader/Header.vue
@@ -1,35 +1,15 @@
 <template>
   <div id="headers">
     <el-row class="black-head">
-      <el-col :span="8">
-        <el-image style="width: 100%; height: 60px" :src="logoUrl" fit="scale-down"></el-image>
-      </el-col>
-      <el-col :span="16">
-        <el-menu
-          :default-active="activeIndex"
-          class="el-menu-demo"
-          mode="horizontal"
-          @select="handleSelect"
-        >
-          <el-menu-item index="1">处理中心</el-menu-item>
-          <el-submenu index="2">
-            <template slot="title">我的工作台</template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-            <el-menu-item index="2-3">选项3</el-menu-item>
-            <el-submenu index="2-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="2-4-1">选项1</el-menu-item>
-              <el-menu-item index="2-4-2">选项2</el-menu-item>
-              <el-menu-item index="2-4-3">选项3</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-          <el-menu-item index="3" disabled>消息中心</el-menu-item>
-          <el-menu-item index="4">
-            <a href="https://www.ele.me" target="_blank">订单管理</a>
-          </el-menu-item>
-        </el-menu>
-      </el-col>
+      <el-image class="logo" :src="logoUrl" fit="scale-down"></el-image>
+      <ul class="nav">
+        <li v-for="(item,index) in navData" :key="index">
+          <a :href="item.url">
+            <h2>{{item.cntitle}}</h2>
+            <p>{{item.entitle}}</p>
+          </a>
+        </li>
+      </ul>
     </el-row>
   </div>
 </template>
@@ -39,9 +19,16 @@ export default {
   name: "headers",
   data() {
     return {
-      activeIndex: "1",
-      activeIndex2: "1",
-      logoUrl: require('@/assets/images/logo.png')
+      logoUrl: require("@/assets/images/logo.png"),
+      navData: [
+        { cntitle: "首页", entitle: "HOME", url: "/index" },
+        { cntitle: "产品介绍", entitle: "PRODUCT", url: "/product" },
+        { cntitle: "品牌动态", entitle: "BRAND", url: "/brand" },
+        { cntitle: "基因育发", entitle: "HAIR GENE", url: "/hairgene" },
+        { cntitle: "脱发指南", entitle: "GUIDE", url: "/guide" },
+        { cntitle: "粉丝福利", entitle: "WELFAFE", url: "/welfafe" },
+        { cntitle: "关于我们", entitle: "ABOUT US", url: "/aboutus" }
+      ]
     };
   },
   methods: {
